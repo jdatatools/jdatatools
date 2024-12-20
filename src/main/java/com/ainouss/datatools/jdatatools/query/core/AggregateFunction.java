89fe150da5142fe1c9cc69e9d5cc3b87f76d556d
Abdelghani AINOUSS <abdelghani.ainouss@prima-solutions.com>
2024-12-20 04:23:48 +0100

    clean up

--- src/main/java/com/ainouss/datatools/jdatatools/query/core/AggregateFunction.java
+++ src/main/java/com/ainouss/datatools/jdatatools/query/core/AggregateFunction.java
@@ -3,7 +3,6 @@ package com.ainouss.datatools.jdatatools.query.core;
 public abstract class AggregateFunction extends Alias implements Aggregable {
 
     protected final Selectable selectable;
-    protected String alias;
 
 
     public AggregateFunction(Selectable selectable) {
--- src/main/java/com/ainouss/datatools/jdatatools/query/core/StaticPath.java
+++ src/main/java/com/ainouss/datatools/jdatatools/query/core/StaticPath.java
@@ -3,7 +3,6 @@ package com.ainouss.datatools.jdatatools.query.core;
 public class StaticPath extends Alias implements Selectable {
 
     private final String path;
-    public String alias;
 
     public StaticPath(String path) {
         this.path = path;
