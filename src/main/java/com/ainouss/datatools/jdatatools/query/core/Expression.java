2d6fc7213cb112812913012a76bf362871fa2157
Abdelghani AINOUSS <abdelghani.ainouss@prima-solutions.com>
2024-12-24 09:51:45 +0100

    remove upper case NOT

--- src/main/java/com/ainouss/datatools/jdatatools/query/core/Expression.java
+++ src/main/java/com/ainouss/datatools/jdatatools/query/core/Expression.java
@@ -66,7 +66,7 @@ public abstract class Expression implements Fragment {
         var builder = new StringBuilder(sql());
 
         if (!this.not.isEmpty()) {
-            builder.append(" NOT (")
+            builder.append(" not (")
                     .append(nots)
                     .append(")");
         }
